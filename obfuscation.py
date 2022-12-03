392fcdd5e35a50da395a9732dc476806963b2802
[youngjin] <sdudwls128@naver.com>
2022-12-03 16:05:34 +0900

    add comment obfuscation

--- obfuscation.py
+++ obfuscation.py
@@ -12,41 +12,60 @@ if __name__ == '__main__':
     tot = (p - 1) * (q - 1)
     e = rsa.getPublicKey(tot)
     d = rsa.getPrivateKey(e, tot)
+    public_key = (e, n)
+    private_key = (d, n)
 
-    origin = open(ROOT / 'test.py', 'r+', encoding='utf-8')
-    encrypted = open(ROOT / 'encrypted.py', 'a+', encoding='utf-8')
-    decrypted = open(ROOT / 'decrypted.py', 'a+', encoding='utf-8')
+    origin_file = open(ROOT / 'test.py', 'r+', encoding='utf-8')
+    encrypted_file = open(ROOT / 'encryption_result.py',
+                          'a+', encoding='utf-8')
+    decrypted_file = open(ROOT / 'decryption_result.py',
+                          'a+', encoding='utf-8')
 
-    origin_text = origin.readlines()
+    origin_text = origin_file.readlines()
     encrypted_text = []
     decrypted_text = []
 
+    # 암호화 과정
     for line in origin_text:
         is_encrypt = line.find('encrypt')
         is_def = line.find('def')
+        is_comment = line.find('#')
 
-        if is_encrypt >= 0 and is_def == -1:
+        # 주석 암호화
+        if is_comment >= 0:
+            str = line[is_comment + 1: -1]
+            line = line.replace(str, rsa.encrypt(public_key, str))
+
+        # 암호화하려는 문자열 암호화
+        elif is_def == -1 and is_encrypt >= 0:
             line = line.replace('encrypt', 'decrypt')
             start = line.find('(')
             end = line.find(')')
             str = line[start + 2: end - 1]
-            line = line.replace(str, rsa.encrypt((e, n), str))
+            line = line.replace(str, rsa.encrypt(public_key, str))
 
         encrypted_text.append(line)
 
-    encrypted.write(''.join(encrypted_text))
+    encrypted_file.write(''.join(encrypted_text))
 
+    # 복호화 과정
     for line in encrypted_text:
         is_decrypt = line.find('decrypt')
         is_def = line.find('def')
+        is_comment = line.find('#')
 
-        if is_decrypt >= 0 and is_def == -1:
+        # 주석 복호화
+        if is_comment >= 0:
+            str = line[is_comment + 1: -1]
+            line = line.replace(str, rsa.decrypt(private_key, str))
+
+        elif is_decrypt >= 0 and is_def == -1:
             line = line.replace('decrypt', 'encrypt')
             start = line.find('(')
             end = line.find(')')
             str = line[start + 2: end - 1]
-            line = line.replace(str, rsa.decrypt((d, n), str))
+            line = line.replace(str, rsa.decrypt(private_key, str))
 
         decrypted_text.append(line)
 
-    decrypted.write(''.join(decrypted_text))
+    decrypted_file.write(''.join(decrypted_text))
